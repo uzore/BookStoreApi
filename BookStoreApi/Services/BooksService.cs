9f283798c18409390cae68c6e1be8a927146145f
uzore <narchjosephs@gmail.com>
2023-11-26 06:55:09 +0100

    Add project files.

---
+++ BookStoreApi/Services/BooksService.cs
@@ -0,0 +1,45 @@
+﻿using BookStoreApi.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace BookStoreApi.Services;
+
+public class BooksService
+{
+    private readonly IMongoCollection<Book> _booksCollection;
+
+    public BooksService(
+        IOptions<BookStoreDatabaseSettings> bookStoreDatabaseSettings)
+    {
+
+        if (string.IsNullOrEmpty(bookStoreDatabaseSettings.Value.ConnectionString))
+        {
+            // Handle the case where the connection string is not provided.
+            // You might want to throw an exception, log an error, or take appropriate action.
+            throw new ArgumentException("Connection string is null or empty.");
+        }
+
+        var mongoClient = new MongoClient(bookStoreDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(
+            bookStoreDatabaseSettings.Value.DatabaseName);
+
+        _booksCollection = mongoDatabase.GetCollection<Book>(
+            bookStoreDatabaseSettings.Value.BooksCollectionName);
+    }
+
+    public async Task<List<Book>> GetAsync() =>
+        await _booksCollection.Find(_ => true).ToListAsync();
+
+    public async Task<Book?> GetAsync(string id) =>
+        await _booksCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+    public async Task CreateAsync(Book newBook) =>
+        await _booksCollection.InsertOneAsync(newBook);
+
+    public async Task UpdateAsync(string id, Book updatedBook) =>
+        await _booksCollection.ReplaceOneAsync(x => x.Id == id, updatedBook);
+
+    public async Task RemoveAsync(string id) =>
+        await _booksCollection.DeleteOneAsync(x => x.Id == id);
+}
